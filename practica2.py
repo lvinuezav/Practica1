79bf31a9c528ec9f6851503d9c4b53374fc3cb45
CISCO <lvinueza@eas.ec>
2016-12-05 21:42:41 -0500

    Test

---
+++ practica2.py
@@ -0,0 +1,28 @@
+print("--------- TIPOS DE DATOS ---------")
+#Tipos de datos
+print(type (10))
+print(type(5.5))
+print(type(1+5j))
+
+print(2+5)
+print(2+4)
+print(8**3)
+print((1+5j)+(1+8j))
+
+print("esto es una cadena")
+print ('esto es una cdena con comillas simples')
+print("""
+linea1
+linea2
+linea3
+""")
+
+print(type("esto es una cadena"))
+
+print("cadena1 " + "cadena2")
+
+print("cadena 1"*5)
+
+print(type(True))
+
+print(type(False))
---
+++ practica3.py
@@ -0,0 +1,44 @@
+a=5
+print(a)
+print(type(a))
+a="cinco"
+print(a)
+print(type(a))
+
+Nombre="Frank"
+nombre="Lorenzo"
+print(nombre)
+
+palabra="canción"
+print(palabra)
+
+año="1979"
+print(año)
+
+print(3 & 1)
+print(3 << 1)
+
+a = 10
+a = a + 9
+print(a)
+
+a = 10
+a+=9
+print(a)
+
+
+nombre1='frank'
+nombre2='Frank'
+print(nombre1 == nombre2)
+
+anio_nacimiento=1986
+print ("Mayor de edad")
+print ((2016 - anio_nacimiento) >= 18)
+
+edad=15
+print("Adolexcente ?? 12 - 18  ")
+print (edad >=12 and edad <= 18)
+print(12 <= edad <=18)
+
+print("Edad no laboral ???")
+print(1 <= edad <=18 or 80 <=edad <=120)
