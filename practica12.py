8ca6b5e4c67d0c37d99ca66ff4528baeacc96903
lvinueza <lvinueza@eas.ec>
2016-12-14 19:58:32 -0500

    Test

--- practica12.py
+++ practica12.py
@@ -1,2 +1,14 @@
+from ubidots import ApiClient
 import random
-while
+import time
+
+api = ApiClient(token='aw3vYXYy13i9xVl75Zt1jQAsQpde8j')
+my_variable = api.get_variable('5848c8d47625422c4ec9767e')
+
+dato = 100
+
+while dato > 50:
+    dato = random.randrange(40, 80)
+    print (dato)
+    new_value = my_variable.save_value({'value': dato})
+    time.sleep(2)
