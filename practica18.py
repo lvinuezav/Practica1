250eed4dc797caec6dfcf577784bc66890f952ef
Lorenzo <lvinueza@eas.ec>
2016-12-21 18:54:34 -0500

    Test

---
+++ practica18.py
@@ -0,0 +1,18 @@
+import numpy as np
+fila = 1
+columna = 2
+
+matriz = [[1,3,6],
+    [5,7,9],
+    [3,18,0]]
+
+print (matriz[fila][columna])
+
+arreglo = np.array(matriz)
+print (arreglo)
+
+valor_max = np.max(arreglo)
+print (valor_max)
+
+matriz = np.random.randint(1,50,(10,5))
+print (matriz)
